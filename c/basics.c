36b9ce38dbf0b14eb48c05f2cbd052db56d7b46f
ethan <2026whitney.e@fcboe.org>
2024-01-22 14:48:03 -0500

    c basics file

---
+++ c/basics.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+        printf("this is my c basics file \n");
+
+	int num;
+        
+	printf("enter a number"); 
+        scanf("%i", &num);
+
+	printf("Your number was %i/n", num);
+        return 0;        
+} 
